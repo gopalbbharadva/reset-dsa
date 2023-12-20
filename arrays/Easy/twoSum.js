7c48c010e7a188041739f5912332209b8f2cd0d7
gopalbbharadva <gopalbbharadva@gmail.com>
2023-12-20 15:17:00 +0530

    Feat: optimal approach for two sum problem added

--- arrays/Easy/twoSum.js
+++ arrays/Easy/twoSum.js
@@ -15,17 +15,34 @@
 // console.log(isTwoSumAvailable([2, 6, 5, 8, 11], 14), 'result')
 
 // Better approach
+// const twoSumBetterApproach = (arr, target) => {
+//   const map = new Map()
+//   for (let i = 0; i < arr.length; i++) {
+//     const temp = target - arr[i]
+//     if (map.has(temp)) {
+//       return [map.get(temp), i]
+//     } else {
+//       map.set(arr[i], i)
+//     }
+//   }
+//   return false
+// }
+
+// console.log(twoSumBetterApproach([2, 3, 4, 5, 6, 8, 11], 19), 'res')
+
+// Optimal approach - this approach will work only for sorted array
 const twoSumOptimalApproach = (arr, target) => {
-  const map = new Map()
-  for (let i = 0; i < arr.length; i++) {
-    const temp = target - arr[i]
-    if (map.has(temp)) {
-      return [map.get(temp), i]
+  let i = 0,
+    j = arr.length - 1
+  while (arr[i] + arr[j] !== target) {
+    const temp = arr[i] + arr[j]
+    if (temp > target) {
+      j--
     } else {
-      map.set(arr[i], i)
+      i++
     }
   }
-  return false
+  return [i, j]
 }
 
-console.log(twoSumOptimalApproach([2, 3, 4, 5, 6, 8, 11], 19), 'res')
+console.log(twoSumOptimalApproach([4, 6, 7, 8, 10], 15))
