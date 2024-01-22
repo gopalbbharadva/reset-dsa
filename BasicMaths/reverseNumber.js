a2c37fcdb8a5b2333cbdc07bebb3de1f3bb81080
gopalbbharadva <gopalbbharadva@gmail.com>
2024-01-22 20:19:27 +0530

    Feat: reverse a number

---
+++ BasicMaths/reverseNumber.js
@@ -0,0 +1,19 @@
+// Problem Statement: Given a number N reverse the number and print it.
+// Example 1:
+// Input: N = 123
+// Output: 321
+// Explanation: The reverse of 123 is 321
+
+const reverseNumber = (n) => {
+  let temp = 0
+  const max = Math.pow(2, 31) - 1
+  const min = -Math.pow(2, 31)
+  while (n != 0) {
+    const rem = n % 10
+    temp = temp * 10 + rem
+    n = (n / 10).toString().split('.')[0]
+  }
+  return temp <= max && temp >= min ? temp : n
+}
+
+console.log(reverseNumber(1534236469), 'temp')
