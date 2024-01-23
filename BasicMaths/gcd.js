72bb16496f50d54b43c083d33ca1e6306d87ecc8
gopalbbharadva <gopalbbharadva@gmail.com>
2024-01-23 20:50:54 +0530

    Feat: find gcd question

--- BasicMaths/gcd.js
+++ BasicMaths/gcd.js
@@ -7,14 +7,12 @@
 
 const findGCD = (n1, n2) => {
   let biggerFactor = 0
-  for (let i = 0; i < 10; i++) {
+  for (let i = 1; i <= Math.min(n1, n2); i++) {
     if (n1 % i === 0 && n2 % i === 0) {
-      if (biggerFactor < i) {
-        biggerFactor = i
-      }
+      biggerFactor = i
     }
   }
   return biggerFactor
 }
 
-console.log(findGCD(3, 6), 'res')
+console.log(findGCD(4, 8), 'res')
