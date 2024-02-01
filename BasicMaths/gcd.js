5972c7760d157a90e7ad776ea913b92f6767e6aa
gopalbbharadva <gopalbbharadva@gmail.com>
2024-02-01 19:43:51 +0530

    Feat: gcd with recursion

--- BasicMaths/gcd.js
+++ BasicMaths/gcd.js
@@ -5,6 +5,7 @@
 // Output: 4
 // Explanation: Since 4 is the greatest number which divides both num1 and num2.
 
+// Brute force
 const findGCD = (n1, n2) => {
   let biggerFactor = 0
   for (let i = 1; i <= Math.min(n1, n2); i++) {
@@ -15,4 +16,42 @@ const findGCD = (n1, n2) => {
   return biggerFactor
 }
 
-console.log(findGCD(4, 8), 'res')
+// Optimal
+const findGCDWithOptimalApproach = (n1, n2) => {
+  let temp = 0
+  while (n1 > 0 || n2 > 0) {
+    if (n1 > n2) {
+      n1 = n1 % n2
+    } else {
+      n2 = n2 % n1
+    }
+    if (n1 === 0) {
+      temp = n2
+      break
+    }
+    if (n2 === 0) {
+      temp = n1
+      break
+    }
+  }
+  return temp
+}
+
+// console.log(findGCDWithOptimalApproach(5, 10), '***')
+
+const findGCDWithRecursion = (n1, n2) => {
+  // let
+  if (n1 === 0 || n2 === 0) {
+    return n1
+  } else {
+    if (n1 > n2) {
+      return findGCDWithRecursion(n2, n1 % n2)
+    } else {
+      return findGCDWithRecursion(n1, n2 % n1)
+    }
+  }
+}
+
+console.log(findGCDWithRecursion(22, 33))
+
+// console.log(findGCD(4, 8), 'res')
---
+++ BasicMaths/printAllDivisor.js
@@ -0,0 +1,13 @@
+// Problem Statement: Given a number, check if it is Armstrong Number or Not.
+
+// Example 1:
+// Input: n = 36
+// Output: 1 2 3 4 6 9 12 18 36
+// Explanation: All the divisors of 36 are printed.
+
+const printAllDivisor = (n) => {
+  let temp = []
+  for (let i = 0; i < n / 2; i++) {}
+}
+
+printAllDivisor(10)
