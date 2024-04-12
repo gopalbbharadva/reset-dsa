f864a1669554e5c3ac2349b5e740b778f0e0c474
gopalbbharadva <gopalbbharadva@gmail.com>
2024-04-12 17:53:29 +0530

    Feat:reverse the string

--- strings/reverseString.js
+++ strings/reverseString.js
@@ -1,5 +1,6 @@
 // Problem : Reverse a given string
 
+// 1st approach
 const reverseString = (strArr) => {
   let temp = ''
   for (let i = strArr.length - 1; i >= 0; i--) {
@@ -8,4 +9,18 @@ const reverseString = (strArr) => {
   return temp
 }
 
-console.log(reverseString('hello'), 'result')
+// 2nd approach
+const reverseString2 = (str) => {
+  let left = 0,
+    right = str.length - 1
+  while (left < right) {
+    const temp = str[right]
+    str[right] = str[left]
+    str[left] = temp
+    right--
+    left++
+  }
+  return str
+}
+
+console.log(reverseString2(['h', 'e', 'l', 'l', 'o']), 'result')
