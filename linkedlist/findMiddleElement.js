acdefd1ba7a89a1aecb0dc97179ca0f8a87c357f
gopalbbharadva <gopalbbharadva@gmail.com>
2024-05-10 19:25:45 +0530

    Refactor: removed the unused code

--- linkedlist/findMiddleElement.js
+++ linkedlist/findMiddleElement.js
@@ -18,60 +18,22 @@ n1.next = n2
 n2.next = n3
 n3.next = n4
 // n4.next = n5
-// n5.next = n1
-
-// TC: O(2n)
-// SC: O(n)
-
-const oddEvenElement = () => {
-  let temp = head
-  let arr = []
-
-  // FOR ODD indexes
-  while (temp !== null && temp.next !== null) {
-    arr.push(temp.data)
-    temp = temp.next.next
-  }
-  if (temp) {
-    arr.push(temp.data)
-  }
-
-  // FOR EVEN indexes
-  temp = head.next
-  while (temp !== null && temp.next !== null) {
-    console.log('temp')
-    arr.push(temp.data)
-    temp = temp.next.next
-  }
-  if (temp) {
-    arr.push(temp.data)
-  }
-
-  //  For rearranging in existing linked list
-  temp = head
-  for (let i = 0; i < arr.length; i++) {
-    temp.data = arr[i]
-    temp = temp.next
-  }
-  return head
-}
-
-oddEvenElement()
+n2.next = n1
 
 // Tortoise and hare algorithm,
-// const findMiddleElement = () => {
-//   let slow = head,
-//     fast = head
-//   // Whenever fast will reach at the end we will stop
-//   while (fast !== null) {
-//     console.log('first')
-//     // Moving fast two times
-//     fast = fast.next.next
-//     // Moving slow one time
-//     slow = slow.next
-//     if (slow === fast) return true
-//   }
-//   return false
-// }
+const findMiddleElement = () => {
+  let slow = head,
+    fast = head
+  // Whenever fast will reach at the end we will stop
+  while (fast !== null && fast.next !== null) {
+    console.log('first')
+    // Moving fast two times
+    fast = fast.next.next
+    // Moving slow one time
+    slow = slow.next
+    if (slow === fast) return true
+  }
+  return false
+}
 
-// console.log(findMiddleElement(), 'middle')
+console.log(findMiddleElement(), 'middle')
