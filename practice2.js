eca6005f3307a19cede4e0fd3995ad6dc15c2d3a
gopalbbharadva <gopal.bharadva@finbox.in>
2024-07-08 23:06:57 +0530

    Feat: Solving basic mathematics problems

---
+++ practice2.js
@@ -0,0 +1,30 @@
+//1. Problem Statement: Given an integer N, return the number of digits in N.
+
+// const countDigits = (n) => {
+//   let count = 0
+//   while (n > 0) {
+//     n = Math.round(n / 10)
+//     count++
+//   }
+//   return count
+// }
+// const res = countDigits(1234)
+// console.log(res, 'res')
+
+// *************
+//2. Problem Statement: Given an integer N return the reverse of the given number.
+
+// const reverseNumber = (n) => {
+//   let temp = n
+//   let revNum = 0
+//   while (temp > 0) {
+//     const t = temp % 10
+//     revNum = revNum * 10 + t
+//     temp = Math.floor(temp / 10)
+//   }
+//   return revNum
+// }
+
+// const res = reverseNumber(10000)
+
+// *************
