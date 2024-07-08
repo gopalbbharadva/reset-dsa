afd770cff68b4bc40875016cf89ae18fa72a57d6
gopalbbharadva <gopal.bharadva@finbox.in>
2024-07-08 23:43:15 +0530

    Feat: Solved gcd problem with better and brute force approach

--- mathProblems.js
+++ mathProblems.js
@@ -45,3 +45,44 @@
 // }
 
 // const res = isNumberPalindrome(343)
+
+// *************
+//4. Problem Statement: Given two integers N1 and N2, find their greatest common divisor.
+
+// !! Brute Force approach
+// const findGcd = (n1, n2) => {
+//   const minValue = Math.min(n1, n2)
+//   let gcd = 0
+//   for (let i = 1; i <= minValue; i++) {
+//     if (n1 % i === 0 && n2 % i === 0) {
+//       if (gcd < i) {
+//         gcd = i
+//       }
+//     }
+//   }
+//   return gcd
+// }
+
+// const res = findGcd(5, 13)
+
+// !! Better approach
+// && Explanation
+/**
+ * As we are finding the greatest common divisor, our gcd will be
+ * near to the minimum(n1,n2)
+ * For ex: n1= 5, n2=15, GCD =5 will near to the 5 instead of 1 ,
+ * so we can iterate from minimum to 1. so our iterations will reduce.
+ */
+
+// const findGcdBetterApproach = (n1, n2) => {
+//   const minValue = Math.min(n1, n2)
+//   for (let i = minValue; i >= 1; i--) {
+//     if (n1 % i == 0 && n2 % i == 0) {
+//       return i
+//     }
+//   }
+//   return 1
+// }
+
+// const res = findGcdBetterApproach(5, 15)
+// console.log(res, 'res')
