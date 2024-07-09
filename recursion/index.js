bd941d96d54b64b659292e73273aaaa79e64c9ed
gopalbbharadva <gopal.bharadva@finbox.in>
2024-07-10 00:27:43 +0530

    Feat: recursion basic questions solved.

---
+++ recursion/index.js
@@ -0,0 +1,51 @@
+// !! 1. Print something n times using recursion
+// const printUsingRecursion = (num) => {
+//   if (num === 0) {
+//     return
+//   } else {
+//     console.log('hello')
+//     printUsingRecursion(num - 1)
+//   }
+// }
+
+// printUsingRecursion(5)
+
+// !! 2. Print 1 to N using recursion
+
+// const print1ToN = (num) => {
+//   if (num === 0) {
+//     return
+//   } else {
+//     print1ToN(num - 1)
+//     console.log(num, 'num')
+//   }
+// }
+
+// print1ToN(5)
+
+// !! 3. Print N to 1 using recursion
+
+// const printNTo1 = (num) => {
+//   if (num === 0) {
+//     return
+//   } else {
+//     console.log(num, 'num')
+//     printNTo1(num - 1)
+//   }
+// }
+
+// printNTo1(5)
+
+// !! 4. Sum of first N numbers
+
+// const sumOfNNumbers = (num) => {
+//   if (num === 1) {
+//     return num
+//   } else {
+//     console.log(num, 'num')
+//     return num + sumOfNNumbers(num - 1)
+//   }
+// }
+
+// const res = sumOfNNumbers(5)
+// console.log(res, 'res')
